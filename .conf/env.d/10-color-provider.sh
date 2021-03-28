f0767e8e205dde33063d8b357e07057804f53d06
Hannah Lappe <hannah.lappe0@gmail.com>
2021-03-28 15:55:24 +0200

    auto update

---
+++ .conf/env.d/10-color-provider.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+case $THEME in
+  dark-cyan)
+    BLACK='212121'
+    LIGHT_GRAY='eeffff'
+    DARK_GRAY='4a4a4a'
+    WHITE='ffffff'
+    RED='f07178'
+    GREEN='c3e88d'
+    YELLOW='ffcb6b'
+    BLUE='82aaff'
+    MAGENTA='c792ea'
+    CYAN='89ddff'
+
+    PRIMARY=$CYAN
+    ;;
+  *)
+    echo "THEME="$THEME" unknown or not set"
+    exit 1
+esac
