f263f3a19a87966369b387ec189d00f4275a19c3
Hannah Lappe <hannah.lappe0@gmail.com>
2021-03-28 16:42:46 +0200

    auto update

--- .conf/env.d/10-color-provider.sh
+++ .conf/env.d/10-color-provider.sh
@@ -6,6 +6,7 @@ case $THEME in
     LIGHT_GRAY='eeffff'
     DARK_GRAY='4a4a4a'
     WHITE='ffffff'
+
     RED='f07178'
     GREEN='c3e88d'
     YELLOW='ffcb6b'
@@ -14,6 +15,7 @@ case $THEME in
     CYAN='89ddff'
 
     PRIMARY=$CYAN
+    SECONDARY=$MAGENTA
     ;;
   *)
     echo "THEME="$THEME" unknown or not set"
