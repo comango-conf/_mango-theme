e7031e905b6b90c1968494f620a04751337f2ecf
Hannah Lappe <hannah.lappe0@gmail.com>
2021-03-28 18:46:31 +0200

    auto update

--- .conf/env.d/10-color-provider.sh
+++ .conf/env.d/10-color-provider.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
+
+BLACK='212121'
+LIGHT_GRAY='eeffff'
+DARK_GRAY='4a4a4a'
+WHITE='ffffff'
+
+RED='f07178'
+GREEN='c3e88d'
+YELLOW='ffcb6b'
+BLUE='82aaff'
+MAGENTA='c792ea'
+CYAN='89ddff'
+
 case $THEME in
   dark-cyan)
-    BLACK='212121'
-    LIGHT_GRAY='eeffff'
-    DARK_GRAY='4a4a4a'
-    WHITE='ffffff'
-
-    RED='f07178'
-    GREEN='c3e88d'
-    YELLOW='ffcb6b'
-    BLUE='82aaff'
-    MAGENTA='c792ea'
-    CYAN='89ddff'
-
+    BACKGROUND=$BLACK
+    FOREGROUND=$WHITE
     PRIMARY=$CYAN
     SECONDARY=$MAGENTA
     ;;
+  light-pink)
+    BACKGROUND=$WHITE
+    FOREGROUND=$BLACK
+    PRIMARY=$RED
+    SECONDARY=$MAGENTA
   *)
     echo "THEME="$THEME" unknown or not set"
     exit 1
